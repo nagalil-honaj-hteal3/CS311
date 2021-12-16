3162bb1ddb2854d0557759504d4c090aeff3a1d0
nagalil-honaj-hteal3 <96222174+nagalil-honaj-hteal3@users.noreply.github.com>
2021-12-15 21:35:17 -0800

    Create mergeSort.cpp
    Merge sort
    Invented by John von Neumann
    Out-of-place sorting algorithm – requires an auxiliary array – O(n) extra space
    Divide and conquer algorithm.
    Data structures and algorithms – textbook in reserve at library
    Pseudocode for mergeSort()
    mergeSort(array, first, last)
    if there are at least 2 elements //if there is only one element, return to the caller immediately.
    calculate middle = (first + last)/2 //the left half goes from first and mid inclusive. [first .. mid]
    mergeSort() on left half
    mergeSort() on right half
    merge() //merge the left and right halves into one sorted array
    merge(array, first, last) //in the end of this function, array[first..last] will be sorted
    make temp array dynamically with the exact number of slots you need
    fill temp array with all numbers from left and right halves in order à more implementation info on next page
    //To start
    // left finger points to the first slot of left half = first
    // right finger points to the first slot of right half = (first + last) /2 + 1
    load the content of temp to array mid
    The main idea of merge sort is to
    merge sorted halves of an array
    into one sorted array. These
    halves have to be sorted first,
    which is accomplished by
    merging the already sorted
    halves of these halves. The
    process of dividing arrays into 2
    halves stops when the array has
    fewer than 2 elements.
    first mid last
    e.g.
    merge(array, 5, 8) [1, 4, 6, 8, 10, 3, 5, 2, 22]
    left right
    temp [][][][] 4 slots
    [1, 4, 6, 8, 10, 3, 5, 2, 22]
    left right
    3 > 2
    temp [2][][][]
    [1, 4, 6, 8, 10, 3, 5, 2, 22]
    left right
    3 < 22 mid last
    temp [2][3][][]
    [1, 4, 6, 8, 10, 3, 5, 2, 22]
    left right
    5 < 22 mid last
    temp [2][3][5][]
    [1, 4, 6, 8, 10, 3, 5, 2, 22]
    left right
    left went outside the left half [first .. mid]
    Copy the remainder of elements from the right half.
    temp [2][3][5][22]
    load the content of temp to array
    [1, 4, 6, 8, 10, 3, 5, 2, 22] à [1, 4, 6, 8, 10, 2, 3, 5, 22]
    FYI, I am getting out of my loop once left or
    right goes over its boundary. (If right goes
    over the boundary of the right half, that
    means you copied all elements from the
    right half to temp. Now copy the remaining
    elements from the left half to temp.
    I have a separate loop to copy the remaining
    values from left half and another separate
    loop to copy the remining values from right
    half.
    (You can implement any way you want to
    copy the remainders.)
    Time complexity
    Best and Worst O(n log2n)
    Space complexity
    Auxiliary space O(N)
    Runtime stack space O(log2n) The runtime stack grows as high as (log2n + 1)

---
+++ mergeSort.cpp
@@ -0,0 +1,84 @@
+/**********************************
+Your name: XXXXXXXXXXXXXXXXXXXXXXXX
+Your programmer number: XXXXX
+Any difficulties?: XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+***********************************/
+#include <iostream>
+using namespace std;
+
+/***********************************
+This print array is used to output the following elements in the sorted algorithm from the array and will have a space to separate the following elements.
+***********************************/
+void printAr(const int ar[], int s)
+{
+ for(int i = 0; i < s; i++)
+    cout << ar[i] << " ";
+  cout << endl;
+}
+/***********************************
+The merge function is used to scan the array and split it into twos, to get the left side and right side, and use the left and right fingers to initialize the locations of the array. It would be split by having a middle varaiable that would half the array and continue to half until the size of the array left to compare is 1, it will merge the following elements into a dynamic array, which would be a new array with a new sorting array from least to greatest. Once it completes merging, it will go back to the other elements that are not merged and will continue the same process until all the elements are stored into the temp array in the correct order.
+***********************************/
+void merge(int ar[], int first, int last)
+{
+  int* temp = new int[first + last];//make a dynamic array named temp that would fill numbers from left and right halves in order
+  int mid = (first + last) / 2;
+  int right = mid + 1;//right finger points to the first slot of right half
+  int left = first;//left finger points to the first slot of the left half
+  int start = 0;//used for an initial value for a counter
+
+  while(left <= mid && right <= last)//when the first half of the array is less than or equal to the middle index of the array and the second half of the array is less than or equal to the last index of the array
+    {
+      if(ar[left] <= ar[right])//if the left element is less than the right element
+	temp[start++] = ar[left++];//set the array to the left, if it needs sorting
+      else
+	temp[start++] = ar[right++];//set the array to the right, if it needs sorting
+    }
+  while(left <= mid)//comparing the left (first half) with the middle
+    {
+      temp[start++] = ar[left++];//setting the original array with the temporary array that will be used for sorting on the left side
+    }
+  while(right <= last)//comparing the second half with the last index of the array
+    {
+      temp[start++] = ar[right++];//same as the while loop above but with the right side/second half of the array
+    }
+  start--;//decrement once that part of the array/index was sorted and merged
+  while(start >= 0)//when there are other parts of the array to be checked 
+    {
+      ar[start + first] = temp[start];//starts to merge with the index that is being located 
+      start--;//decrements once the element has been sorted at the correct spot
+    }
+  //For implementation of this function, refer to my lecture notes "merge sort"
+  //If you are using a different technique, I assume you cheated and will give you zero points.
+}
+/***********************************
+The mergeSort function is used to be set recursively when going through the array and will check each sides of the array to get the element from the merge function once it checks the position of the sorted array. It will have a variable called mid that will be used to locate the middle of the array and set it for the two following mergeSort functions to have the elements be sorted out. Once completed with one part of the portion, it will continue to merge until every part of the array has been sorted completely.
+***********************************/
+void mergeSort(int ar[], int first, int last)
+{
+  int mid;
+  if(first < last)//if the first element is less than the last element of the array
+    {
+      mid = (first + last) / 2;//get the mid value of the array
+      mergeSort(ar, first, mid);//on left half
+      mergeSort(ar, mid + 1, last);//on right half
+      merge(ar, first, last);//merge the left and right halves into one sorted array
+    }
+  //For implementation of this function, refer to my lecture notes "merge sort"
+}
+
+int main()
+{
+  int ar[] = {1, 8, 6, 4, 10, 5, 3, 2, 22}; //the array used in my lecture notes
+  mergeSort(ar, 0, 8);
+  printAr(ar, 9); //1 2 3 4 5 6 8 10 22
+
+  int ar1[] = {1, 7, 5, 3, 9, 4};
+  mergeSort(ar1, 0, 5);
+  printAr(ar1, 6); //1 3 4 5 7 9 
+
+  int ar2[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+  mergeSort(ar2, 0, 9);
+  printAr(ar2, 10); //1 2 3 4 5 6 7 8 9 10
+
+  return 0;
+}
