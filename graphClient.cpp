1e4435d5a47c9f2127bc37d8c5fa19a2db8c1a4b
nagalil-honaj-hteal3 <96222174+nagalil-honaj-hteal3@users.noreply.github.com>
2022-05-21 19:02:56 -0700

    Create graphClient.cpp

---
+++ graphClient.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+#include "graph.h"
+
+int main()                                                                     \
+
+{
+  cout << "testing unweighted graph--------------------" << endl;
+                                                                               \
+
+  graph g(9); //a to i
+
+  ifstream fin;
+  fin.open("graph.in");
+  int v, u, w; //v --> u
+               //  w
+
+  if(!fin)
+    cout << "graph.in doesn't exist" << endl;
+  else
+    {
+      fin >> v >> u;
+      while(fin)
+        {
+          g.addEdge(v,u);
+          fin >> v >> u;
+        }
+
+      cout << "BFS: ";                                                         \
+
+      g.BFT(0);                                                                \
+
+      cout << "DFS: ";                                                         \
+
+      g.DFT(0);
+    }                                                                          \
+
+  fin.close();
+
+  cout << "testing weighted graph--------------------" << endl;
+  graph g2(10); //a to j
+  fin.open("graph2.in");
+  if(!fin)
+    cout << "graph2.in doesn't exist" << endl;
+  else
+    {
+      fin >> v >> u >> w;
+      while(fin)
+        {
+          g2.addEdge(v,u, w);
+          fin >> v >> u >> w;
+        }
+
+      cout << "BFS: ";
+      g2.BFT(0);
+      cout << "DFS: ";
+      g2.DFT(0);
+      cout << "Shortest Path: ";
+      g2.shortestPath(3);
+    }
+  fin.close();                                                                 \
+
+  return 0;                                                                    \
+
+}
